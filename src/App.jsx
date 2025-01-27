3db45850834f8ab6d0c036b66cca511cc0e78695
Manzarabbas110 <manzarmallah00@gmail.com>
2025-01-27 19:07:18 +0500

    new dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
 import React from 'react'
-import Header from './Components/Header/Header'
-import HeroSection from './Components/Herosection/Herosecttion'
-import './App.css'
-import AboutMe  from './Components/AboutMe/Aboutme'
-import Services from './Components/Services/Services'
-import MyWork from './Components/MyWork/Mywork'
-import Form from './Components/Form/Forms'
-
+import 'App.css'
 const App = () => {
   return (
     <div>
-      <Header />
-      <div className='main-component-wrapper'>
-        <HeroSection />
-        <div className='about-me-main'>
-          <AboutMe />
-        </div>
-        <Services />
-        <MyWork />
-        <Form />
-      </div>
+      <Aboutme />
     </div>
   )
 }
